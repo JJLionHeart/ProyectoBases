b8e88d10eae352f08f494a417ce8cecddd53f797
HumbertoG <jhgm_1995@hotmail.com>
2016-05-09 12:26:12 -0500

    medico

--- medico.php
+++ medico.php
@@ -1,4 +1,9 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<?php
+include_once('conexionBD.php');
+$resultado_busquedaSP = mysqli_query($conexion, "CALL ListaDoctores") or die("ERROR EN EL QUERY: " . mysqli_error());
+
+?>
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
@@ -41,19 +46,43 @@
 </div><!---header-wrap--->
 <div class="page">
   <div class="panel">
-    <div class="title">
-      <h1>About</h1>
-      <h2>Nulla elit est, commodo id ultrices et, sollicitudin a odioras tortor ante, placerat at posuer</h2>
-    </div>
-    <div class="content"> 
-		<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Phasellus hendrerit. Pellentesque aliquet nibh nec urna. In nisi neque, aliquet vel, dapibus id, mattis vel, nisi. Sed pretium, ligula sollicitudin laoreet viverra, tortor libero sodales leo, eget blandit nunc tortor eu nibh. Nullam mollis. Ut justo. Suspendisse potenti.</p>
-		<p>Sed egestas, ante et vulputate volutpat, eros pede semper est, vitae luctus metus libero eu augue. Morbi purus libero, faucibus adipiscing, commodo quis, gravida id, est. Sed lectus. Praesent elementum hendrerit tortor. Sed semper lorem at felis. Vestibulum volutpat, lacus a ultrices sagittis, mi neque euismod dui, eu pulvinar nunc sapien ornare nisl. Phasellus pede arcu, dapibus eu, fermentum et, dapibus sed, urna.</p>
-        <p>Morbi interdum mollis sapien. Sed ac risus. Phasellus lacinia, magna a ullamcorper laoreet, lectus arcu pulvinar risus, vitae facilisis libero dolor a purus. Sed vel lacus. Mauris nibh felis, adipiscing varius, adipiscing in, lacinia vel, tellus. Suspendisse ac urna. Etiam pellentesque mauris ut lectus. Nunc tellus ante, mattis eget, gravida vitae, ultricies ac, leo. Integer leo pede, ornare a, lacinia eu, vulputate vel, nisl.</p>
-        <p>Suspendisse mauris. Fusce accumsan mollis eros. Pellentesque a diam sit amet mi ullamcorper vehicula. Integer adipiscing risus a sem. Nullam quis massa sit amet nibh viverra malesuada. Nunc sem lacus, accumsan quis, faucibus non, congue vel, arcu. Ut scelerisque hendrerit tellus. Integer sagittis. Vivamus a mauris eget arcu gravida tristique. Nunc iaculis mi in ante. Vivamus imperdiet nibh feugiat est.</p>
-        <p>Suspendisse mauris. Fusce accumsan mollis eros. Pellentesque a diam sit amet mi ullamcorper vehicula. Integer adipiscing risus a sem. Nullam quis massa sit amet nibh viverra malesuada. Nunc sem lacus, accumsan quis, faucibus non, congue vel, arcu. Ut scelerisque hendrerit tellus. Integer sagittis. Vivamus a mauris eget arcu gravida tristique. Nunc iaculis mi in ante. Vivamus imperdiet nibh feugiat est.</p>
-        <p>Ut convallis, sem sit amet interdum consectetuer, odio augue aliquam leo, nec dapibus tortor nibh sed augue. Integer eu magna sit amet metus fermentum posuere. Morbi sit amet nulla sed dolor elementum imperdiet. Quisque fermentum. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Pellentesque adipiscing eros ut libero. Ut condimentum mi vel tellus. Suspendisse laoreet. Fusce ut est sed dolor gravida convallis. Morbi vitae ante. Vivamus ultrices luctus nunc. Suspendisse et dolor. Etiam dignissim. Proin malesuada adipiscing lacus. Donec metus. Curabitur gravida.</p>
-    </div>
-  </div>
+	<div class = "content">
+	 <center><h1>Lista de doctores registrados en la base de datos </h1></center>
+	 <br>
+	 <br>
+	<table>
+	<thead>
+		<tr>
+			<th width = "85">ID</th>
+			<th width = "120">Nombre</th>
+			<th width = "150">Apellido</th>
+            <th width = "70">Genero</th>
+	    	<th>Fecha de Nacimiento</th>
+			<th>Tipo de sangre</th>
+            <th width = "85">Teléfono</th>
+		    <th>Nombre del seguro</th>
+		</tr>
+	</thead>
+	<tbody>
+	<?php 
+		if (isset($resultado_busquedaSP)) {
+			while ($row =  mysqli_fetch_array($resultado_busquedaSP)) {
+				echo 
+        		"	
+				<tr height = \"50\">
+				<td align = \"center\"><a href=\"editar_entidad.php?ssn=".$row['DoctorID ']."\t\">".$row['DoctorID '] ."</a></td>
+				<td align = \"center\">".$row['Nombre'] ."</td>
+				<td align = \"center\">".$row['Telefono '] ."</td>
+                <td align = \"center\">".$row['DireccionConsultorio '] ."</td>
+				<td align = \"center\">".$row['Especialidad '] ."</td>
+				</tr>";
+			}
+		}
+	?>
+	</tbody>
+	</table>
+	</div>
+	</div>
 </div>
 <!---page-wrap-end--->
 <div class="footer-wrap">
