13e30ddfd88a8b3028c99b4683dfabd6c5cbf643
Juan José López Jaimez <Juan Jos Lpez Jaimez>
2016-05-09 12:08:27 -0500

    ya lista los pacientes :)

--- paciente.php
+++ paciente.php
@@ -1,4 +1,9 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<?php
+include_once('conexionBD.php');
+$resultado_busquedaSP = mysqli_query($conexion, "CALL ListPacientes") or die("ERROR EN EL QUERY: " . mysqli_error());
+
+?>
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
@@ -49,114 +54,46 @@
 </div><!---header-wrap--->
 <div class="page">
   <div class="panel">
-    <div class="title">
-      <h1>works</h1>
-      <h2>Nulla elit est, commodo id ultrices et, sollicitudin a odioras tortor ante, placerat at posuer</h2>
-    </div>
-    
-    
-    
-    
-    <div class="portfolio-content">
-    <ul class="portfolio-categ filter">
-      <li>Categories:</li>
-      <li class="all active"><a href="#">All</a></li>
-      <li class="cat-item-1"><a href="#" title="Category 1">Category 1</a></li>
-      <li class="cat-item-2"><a href="#" title="Category 2">Category 2</a></li>
-      <li class="cat-item-3"><a href="#" title="Category 3">Category 3</a></li>
-      <li class="cat-item-4"><a href="#" title="Category 4">Category 4</a></li>
-    </ul>
-    <ul class="portfolio-area">
-      <li class="portfolio-item2" data-id="id-0" data-type="cat-item-4">
-        <div> <span class="image-block"> <a class="image-zoom" href="images/big/pic1.jpg" rel="prettyPhoto[gallery]" title="Praesent placerat"><img width="190" height="130" src="images/thumbs/p1.jpg" alt="Praesent placerat" title="Praesent placerat" /> </a> </span>
-          <div class="home-portfolio-text">
-            <h4 class="post-title-portfolio"><a href="#" rel="bookmark" title="Praesent placerat">Praesent placerat</a></h2>
-          </div>
-        </div>
-      </li>
-      <li class="portfolio-item2" data-id="id-1" data-type="cat-item-2">
-        <div> <span class="image-block"> <a class="image-zoom" href="images/big/pic2.jpg" rel="prettyPhoto[gallery]" title="Fusce pellentesque"><img width="190" height="130" src="images/thumbs/p2.jpg" alt="Fusce pellentesque" title="Fusce pellentesque" /> </a> </span>
-          <div class="home-portfolio-text">
-            <h4 class="post-title-portfolio"><a href="#" rel="bookmark" title="Fusce pellentesque">Fusce pellentesque</a></h2>
-          </div>
-        </div>
-      </li>
-      <li class="portfolio-item2" data-id="id-2" data-type="cat-item-1">
-        <div> <span class="image-block"> <a class="image-zoom" href="images/big/pic3.jpg" rel="prettyPhoto[gallery]" title="Vestibulum "><img width="190" height="130" src="images/thumbs/p3.jpg" alt="Vestibulum " title="Vestibulum " /> </a> </span>
-          <div class="home-portfolio-text">
-            <h4 class="post-title-portfolio"><a href="#" rel="bookmark" title="Vestibulum ">Vestibulum </a></h2>
-          </div>
-        </div>
-      </li>
-      <li class="portfolio-item2" data-id="id-3" data-type="cat-item-4">
-        <div> <span class="image-block"> <a class="image-zoom" href="images/big/pic4.jpg" rel="prettyPhoto[gallery]" title="Ut aliquam "><img width="190" height="130" src="images/thumbs/p4.jpg" alt="Ut aliquam " title="Ut aliquam " /> </a> </span>
-          <div class="home-portfolio-text">
-            <h4 class="post-title-portfolio"><a href="#" rel="bookmark" title="Ut aliquam ">Ut aliquam </a></h2>
-          </div>
-        </div>
-      </li>
-      <li class="portfolio-item2" data-id="id-4" data-type="cat-item-3">
-        <div> <span class="image-block"> <a class="image-zoom" href="images/big/pic5.jpg" rel="prettyPhoto[gallery]" title="Cras iaculis ultricies"><img width="190" height="130" src="images/thumbs/p5.jpg" alt="Cras iaculis ultricies" title="Cras iaculis ultricies" /> </a> </span>
-          <div class="home-portfolio-text">
-            <h4 class="post-title-portfolio"><a href="#" rel="bookmark" title="Cras iaculis ultricies">Cras iaculis ultricies</a></h2>
-          </div>
-        </div>
-      </li>
-      <li class="portfolio-item2" data-id="id-5" data-type="cat-item-2">
-        <div> <span class="image-block"> <a class="image-zoom" href="images/big/pic6.jpg" rel="prettyPhoto[gallery]" title="Fusce lobortis"><img width="190" height="130" src="images/thumbs/p6.jpg" alt="Fusce lobortis" title="Fusce lobortis" /> </a> </span>
-          <div class="home-portfolio-text">
-            <h4 class="post-title-portfolio"><a href="#" rel="bookmark" title="Fusce lobortis">Fusce lobortis</a></h2>
-          </div>
-        </div>
-      </li>
-      <li class="portfolio-item2" data-id="id-6" data-type="cat-item-1">
-        <div> <span class="image-block"> <a class="image-zoom" href="images/big/pic7.jpg" rel="prettyPhoto[gallery]" title="Nam convallis"><img width="190" height="130" src="images/thumbs/p7.jpg" alt="Nam convallis" title="Nam convallis" /> </a> </span>
-          <div class="home-portfolio-text">
-            <h4 class="post-title-portfolio"><a href="#" rel="bookmark" title="Nam convallis">Nam convallis</a></h2>
-          </div>
-        </div>
-      </li>
-      <li class="portfolio-item2" data-id="id-7" data-type="cat-item-1">
-        <div> <span class="image-block"> <a class="image-zoom" href="images/big/pic8.jpg" rel="prettyPhoto[gallery]" title="Integer malesuada"><img width="190" height="130" src="images/thumbs/p8.jpg" alt="Integer malesuada" title="Integer malesuada" /> </a> </span>
-          <div class="home-portfolio-text">
-            <h4 class="post-title-portfolio"><a href="#" rel="bookmark" title="Integer malesuada">Integer malesuada</a></h2>
-          </div>
-        </div>
-      </li>
-      <li class="portfolio-item2" data-id="id-8" data-type="cat-item-1">
-        <div> <span class="image-block"> <a class="image-zoom" href="images/big/pic9.jpg" rel="prettyPhoto[gallery]" title="Tempor interdum"><img width="190" height="130" src="images/thumbs/p9.jpg" alt="Tempor interdum" title="Tempor interdum" /> </a> </span>
-          <div class="home-portfolio-text">
-            <h4 class="post-title-portfolio"><a href="#" rel="bookmark" title="Tempor interdum">Tempor interdum</a></h2>
-          </div>
-        </div>
-      </li>
-      <li class="portfolio-item2" data-id="id-9" data-type="cat-item-2">
-        <div> <span class="image-block"> <a class="image-zoom" href="images/big/pic10.jpg" rel="prettyPhoto[gallery]" title="Convallis pellentesque"><img width="190" height="130" src="images/thumbs/p10.jpg" alt="Convallis pellentesque" title="Convallis pellentesque" /> </a> </span>
-          <div class="home-portfolio-text">
-            <h4 class="post-title-portfolio"><a href="#" rel="bookmark" title="Convallis pellentesque">Convallis pellentesque</a></h2>
-          </div>
-        </div>
-      </li>
-      <li class="portfolio-item2" data-id="id-10" data-type="cat-item-3">
-        <div> <span class="image-block"> <a class="image-zoom" href="images/big/pic11.jpg" rel="prettyPhoto[gallery]" title="Commodo nulla"><img width="190" height="130" src="images/thumbs/p11.jpg" alt="Commodo nulla" title="Commodo nulla" /> </a> </span>
-          <div class="home-portfolio-text">
-            <h4 class="post-title-portfolio"><a href="#" rel="bookmark" title="Commodo nulla">Commodo nulla</a></h2>
-          </div>
-        </div>
-      </li>
-      <li class="portfolio-item2" data-id="id-11" data-type="cat-item-4">
-        <div> <span class="image-block"> <a class="image-zoom" href="images/big/pic12.jpg" rel="prettyPhoto[gallery]" title="Egestas placerat"><img width="190" height="130" src="images/thumbs/p12.jpg" alt="Egestas placerat" title="Egestas placerat" /> </a> </span>
-          <div class="home-portfolio-text">
-            <h4 class="post-title-portfolio"><a href="#" rel="bookmark" title="Egestas placerat">Egestas placerat</a></h2>
-          </div>
-        </div>
-      </li>
-      <div class="column-clear"></div>
-    </ul>
-    <!--end portfolio-area -->
-  </div>
-  
-  </div>
+	<div class = "content">
+	 <center><h1>Lista de pacientes registrados en la base de datos </h1></center>
+	 <br>
+	 <br>
+	<table>
+	<thead>
+		<tr>
+			<th width = "85">ID</th>
+			<th width = "120">Nombre</th>
+			<th width = "150">Apellido</th>
+            <th width = "70">Genero</th>
+	    	<th>Fecha de Nacimiento</th>
+			<th>Tipo de sangre</th>
+            <th width = "85">Teléfono</th>
+		    <th>Nombre del seguro</th>
+		</tr>
+	</thead>
+	<tbody>
+	<?php 
+		if (isset($resultado_busquedaSP)) {
+			while ($row =  mysqli_fetch_array($resultado_busquedaSP)) {
+				echo 
+        		"	
+				<tr height = \"50\">
+				<td align = \"center\"><a href=\"editar_entidad.php?ssn=".$row['PacienteID']."\t\">".$row['PacienteID'] ."</a></td>
+				<td align = \"center\">".$row['Nombre'] ."</td>
+				<td align = \"center\">".$row['Apellido'] ."</td>
+                <td align = \"center\">".$row['Genero'] ."</td>
+				<td align = \"center\">".$row['FechaNacimiento'] ."</td>
+				<td align = \"center\">".$row['TipoDeSangre'] ."</td>
+                <td align = \"center\">".$row['Telefono'] ."</td>
+				<td align = \"center\">".$row['NombreSeguro'] ."</td>	
+				</tr>";
+			}
+		}
+	?>
+	</tbody>
+	</table>
+	</div>
+	</div>
 </div>
 <!---page-wrap-end--->
 <div class="footer-wrap">
